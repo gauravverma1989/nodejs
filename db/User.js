eecc3622918b30a106ac10b39008c334e04abd40
unknown <gaurav.v@coppermobile.com>
2024-03-14 16:35:07 +0530

    crud operation added

--- db/User.js
+++ db/User.js
@@ -3,9 +3,10 @@
 
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
+    id: Number,
     Name: String,
     Email: String,
-    Password: String
+    Password: Number
 })
 
 module.exports = mongoose.model('users', UserSchema);
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.listen(3000, () => {
 
 
 // basic get api
-app.get("/user", async(req, res) => {
+app.get("/user", async (req, res) => {
     let result = await user.find();
     let filteredArray = result.find(a => {
         return a.Name == "James Bond"
@@ -24,4 +24,43 @@ app.get("/user", async(req, res) => {
 
     res.send(filteredArray);
     console.log(filteredArray);
+})
+
+// basic get api for single user
+app.get("/user/:Name", async (req, res) => {
+    // findOne for single first single result with params
+    let singleResult = await user.findOne({
+        Name: req.params.Name
+    });
+
+    // find method for all results with params
+    let allResult = await user.find({
+        Name: req.params.Name
+    });
+    res.send(singleResult);
+})
+
+// basic post api for adding user
+app.post("/addUser", async (req, res) => {
+    let userData = new user(req.body);
+    let result = await userData.save();
+    res.send(result)
+})
+
+// basic update api
+app.post("/updateUser/:email", async (req, res) => {
+        console.log("Email params", req.params.email);
+        let result = await user.updateOne(
+            { Email: req.params.email },
+            { $set: req.body }
+        )
+        result ? res.send(result) : res.send({ result: "Something went wrong" })
+})
+
+// basic delete api
+app.delete("/deleteUser/:id", async (req, res) => {
+    let result = await user.deleteOne(
+        { id: req.params.id }
+    )
+    result ? res.send(result) : res.send({ result: "Something went wrong" })
 })
